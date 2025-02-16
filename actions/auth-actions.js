0b86314cd93b006433fe18d593bfc91672a7c540
vaish shivkumar <vaish.1231993.vaishali@gmail.com>
2025-02-16 19:53:55 +0000

    create-auth-sessions

--- actions/auth-actions.js
+++ actions/auth-actions.js
@@ -1,5 +1,6 @@
 'use server';
 
+import { createAuthSession } from "@/lib/auth";
 import { hashUserPassword } from "@/lib/hash";
 import { createUser } from "@/lib/user";
 import { redirect } from "next/navigation";
@@ -28,7 +29,9 @@ export async function signup(prevState, formData) {
     const hashPassword = hashUserPassword(password);
 
     try{
-        createUser(email, hashPassword);
+        const id = createUser(email, hashPassword);
+        await createAuthSession(id)
+        redirect('/training');
     } catch (error) {
         if(error.code === 'SQLITE_CONSTRAINT_UNIQUE'){
             return {
@@ -40,6 +43,4 @@ export async function signup(prevState, formData) {
         throw error;
     }
 
-    redirect('/training');
-    
 }
